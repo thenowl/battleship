f1d783114dffa5694d9e94caa3e0d2ef8b8781fd
thenowl <183875071+thenowl@users.noreply.github.com>
2025-03-12 20:50:05 +0100

    Add placeShip and method to Gameboard and test it

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -3,9 +3,22 @@ import { Ship } from './Ship';
 export class Gameboard {
   #gameBoard;
   #ships;
+  #shipClasses;
   constructor() {
     this.#gameBoard = this.#createGameBoard();
     this.#ships = [];
+    this.#shipClasses = [
+      [5, 'carrier'],
+      [4, 'battleship'],
+      [4, 'battleship'],
+      [3, 'destroyer'],
+      [3, 'destroyer'],
+      [3, 'destroyer'],
+      [2, 'submarine'],
+      [2, 'submarine'],
+      [2, 'submarine'],
+      [2, 'submarine'],
+    ];
   }
 
   #createGameBoard() {
@@ -23,5 +36,65 @@ export class Gameboard {
 
   get gameBoard() {
     return this.#gameBoard;
+  }
+
+  get ships() {
+    return this.#ships;
+  }
+
+  #isShipPlacementValid(ship, x, y, direction) {
+    const shipLength = ship.length;
+
+    if (direction === 'horizontal') {
+      if (x + shipLength > 10) {
+        return false;
+      }
+
+      for (let i = 0; i < shipLength; i++) {
+        if (this.#gameBoard[y][x + i] !== null) {
+          return false;
+        }
+      }
+    } else {
+      if (y + shipLength > 10) {
+        return false;
+      }
+
+      for (let i = 0; i < shipLength; i++) {
+        if (this.#gameBoard[y + i][x] !== null) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  placeShip(shipClass, x, y, direction) {
+    const type = this.#shipClasses.find((ship) => ship[1] === shipClass);
+    this.#shipClasses.splice(this.#shipClasses.indexOf(type), 1);
+
+    const ship = new Ship(type[0]);
+
+    if (this.#isShipPlacementValid(ship, x, y, direction)) {
+      this.#ships.push(ship);
+      const shipLength = ship.length;
+      for (let i = 0; i < shipLength; i++) {
+        if (direction === 'horizontal') {
+          this.#gameBoard[y][x + i] = ship;
+        } else {
+          this.#gameBoard[y + i][x] = ship;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+  receiveAttack(x, y) {
+    if (this.#gameBoard[y][x] !== null) {
+      this.#gameBoard[y][x].hit();
+      return true;
+    }
+    return false;
   }
 }
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect } from '@jest/globals';
 import { Gameboard } from './Gameboard';
+import { Ship } from './Ship';
 
 describe('Gameboard', () => {
   it('should create a gameboard object', () => {
@@ -10,5 +11,31 @@ describe('Gameboard', () => {
   it('should have a gameBoard property', () => {
     const gameBoard = new Gameboard();
     expect(gameBoard.gameBoard).toBeInstanceOf(Array);
+  });
+
+  it('should have a ships property', () => {
+    const gameBoard = new Gameboard();
+    expect(gameBoard.ships).toBeInstanceOf(Array);
+  });
+
+  it('should have a placeShip method', () => {
+    const gameBoard = new Gameboard();
+    expect(gameBoard.placeShip).toBeInstanceOf(Function);
+  });
+
+  it('placeShip method should place a ship', () => {
+    const gameBoard = new Gameboard();
+    gameBoard.placeShip('destroyer', 3, 3, 'horizontal');
+    expect(gameBoard.ships.length).toBe(1);
+    expect(gameBoard.ships[0].length).toBe(3);
+    expect(gameBoard.gameBoard[3][3]).toBeInstanceOf(Ship);
+    expect(gameBoard.gameBoard[3][4]).toBeInstanceOf(Ship);
+    expect(gameBoard.gameBoard[3][5]).toBeInstanceOf(Ship);
+    expect(gameBoard.gameBoard[3][6]).toBe(null);
+  });
+
+  it('should have a receiveAttack method', () => {
+    const gameBoard = new Gameboard();
+    expect(gameBoard.receiveAttack).toBeInstanceOf(Function);
   });
 });
